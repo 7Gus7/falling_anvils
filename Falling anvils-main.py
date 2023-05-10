78f0391e1f285eed9945e16136445378c2d33e5b
Angus <126022900+7Gus7@users.noreply.github.com>
2023-05-09 17:16:56 -0700

    Added menu, added difficulty, added score system, etc
    Finished the game

--- Falling anvils-main.py
+++ Falling anvils-main.py
@@ -1,87 +1,163 @@
-#import radio
-#import music
-
-#import math
-import random
-
-import time
-
 from microbit import *
+import random
 
 
 def main():
-    # are we in the main menu?
-    '''menu = True
-    
-    while menu:
-        show_menu()
-    '''
-
-    x = 2    #players location
-    fall_rate = 5    #falls down one pixel every x seconds
-    spawn_rate = 5    #spawns a new row every x seconds
-    num_spawn = 1    #how many objects are spawned
-    timer = secs(time.ticks_ms())
-    
-    display.set_pixel(x, 4, 9)    #player
-    
+    #Neverending while loop
     while True:
-        if button_a.was_pressed() and x > 0:
-            display.set_pixel(x, 4, 0)
-            x -= 1
-            display.set_pixel(x, 4, 9)
+
+        #Start on the menu until player is ready
+        show_menu()
+
+
         
-        if button_b.was_pressed() and x < 4:
-            display.set_pixel(x, 4, 0)
-            x += 1
-            display.set_pixel(x, 4, 9)
+        #Initialization
+        x = 2             #Players location (starts in middle)
+        fall_rate = 32    #Anvils fall down at this rate
+        spawn_rate = 128  #Spawns a new row at this rate
+        num_spawn = 1     #How many obstacles are spawned
+        score = 0         #Score gained by hitting anvils from the side
+        timer = ticks(running_time())
 
+        #Put the player onscreen
+        display.set_pixel(x, 4, 9)
+        
 
+        
+        #Main gameplay loop
+        while True:
+            
+            #Move player left
+            if button_a.was_pressed() and x > 0:
+                display.set_pixel(x, 4, 0)
+                x -= 1
 
-        if timer != secs(time.ticks_ms()):
-            timer = secs(time.ticks_ms())
+                #If we hit the side of an anvil, increase score
+                if display.get_pixel(x, 4):
+                    score += 1
+                
+                display.set_pixel(x, 4, 9)
 
-            #death check
-            if display.get_pixel(x, 3):
-                print("dead")
+            #Move player right
+            if button_b.was_pressed() and x < 4:
+                display.set_pixel(x, 4, 0)
+                x += 1
 
-            #move anvils down
-            for column in reversed(range(5)):
-                for row in range(5):
-                    if display.get_pixel(row, column) and column != 4:
-                        display.set_pixel(row, column, 0)
-                        display.set_pixel(row, column +1, 9)
-                    elif column == 4 and row != x:
-                        display.set_pixel(row, column, 0)
-
-            #spawn in a new row of anvils
-            if timer % spawn_rate == 0:
-                for led in range(num_spawn):
-                    display.set_pixel(random.randint(0, 4), 0, 9)
-
-
-                        
-
-
-
-
-
-def secs(ms):
-    return ms // 1000
-
-
-
-'''
-def show_menu():
-    display.show("MENU ", delay = 300)
-    display.show("A ", delay = 300)
+                #If we hit the side of an anvil, increase score
+                if display.get_pixel(x, 4):
+                    score += 1
+                
+                display.set_pixel(x, 4, 9)
     
-    # display 'To'
-    display.show(Image('99900:09000:09090:09909:09090'), wait = False)
-    sleep(600)
-    display.show(" Start ", delay = 300)
 
-'''
+            #Timer is updated every centisecond and updates the board
+            if timer != ticks(running_time()):
+                timer = ticks(running_time())
+
+                #Increase the difficulty every so often
+                if timer % 300 == 0:
+                    fall_rate = max(fall_rate//2, 2)
+                    spawn_rate = max(spawn_rate//2, 4)
+                    num_spawn = min(num_spawn + 1, 4)
+                    
+
+                #Every fall_rate seconds, we update the screen
+                if timer % fall_rate == 0:
+                    #Death check
+                    if display.get_pixel(x, 3):
+                        break
+                        
+                    #Move anvils down
+                    fall(x)
+                    
+
+                #Depending on timer, spawn in a row of anvils
+                spawn_row(timer, spawn_rate, num_spawn)
+    
+
+        #You lost, show score and then it 
+        display.scroll("GG Score: " + str(score))
+               
+
+
+
+def ticks(ms):
+    #ms to cs, for consistency in numbers
+    return ms // 100
+
+
+
+
+def show_menu():
+    """
+    Tells the player 'A To Start' and checks if the A button was
+    pressed inbetween words so you aren't just sitting there waiting
+    """
+
+    while True:
+        if button_a.was_pressed():
+            break
+            
+        display.scroll("A")
+        
+        if button_a.was_pressed():
+            break
+
+        #Scrolling image of the word 'to'
+        left_to = Image('99900:09000:09009:09090:09009')
+        right_to = left_to.shift_left(1) + Image('00000:00000:00000:00009:00000')
+        word_to = [left_to.shift_left(-5),
+                   left_to.shift_left(-4),
+                   left_to.shift_left(-3),
+                   left_to.shift_left(-2),
+                   left_to.shift_left(-1),
+                   left_to,
+                   right_to,
+                   right_to.shift_left(1),
+                   right_to.shift_left(2),
+                   right_to.shift_left(3),
+                   right_to.shift_left(4),
+                   right_to.shift_left(5)]
+
+        display.show(word_to, delay=150)
+        if button_a.was_pressed():
+            break
+            
+        display.scroll("Start ")
+
+
+    #Once A is pressed, count down and then begin
+    display.show(321, delay=600)
+    display.clear()
+
+
+
+
+def fall(x):
+    """
+    Goes through each pixel left to right & bottom to top,
+    and shifts them downwards (without removing the player)
+    """
+    for row in reversed(range(5)):
+        for column in range(5):
+            if display.get_pixel(column, row) and row != 4:
+                display.set_pixel(column, row, 0)
+                display.set_pixel(column, row +1, 7)
+            elif row == 4 and column != x:
+                display.set_pixel(column, row, 0)
+
+
+
+
+def spawn_row(timer, spawn_rate, num_spawn):
+    """
+    If it's time to spawn in a new row, we do so. Number of anvils
+    spawned can very, and also they can sometimes stack
+    """
+    if timer % spawn_rate == 0:
+        for led in range(num_spawn):
+            display.set_pixel(random.randint(0, 4), 0, 7)
+
 
 
 
--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # falling_anvils
 
-A little game (currently a work in progress) that has you dodging "anvils from the sky". Built for the BBC micro:bit
-
-
-December ‎2022 - Present
-
-A small project/game which is made for the BBC micro:bit using their online Python code editor. It is a simple game where objects fall from the sky and the player must dodge them.
-
-- Used a variety of micro:bit custom libraries and employed them effectively
-
-- Worked under the constraints of the micro:bit’s hardware which lead to having to get creative with the implementation of ideas
-
-- Repetitively transferring code from the online editor to the physical device, resulting in understanding the boundaries between hardware and software and how they interact
+A little game that has you dodging "anvils from the sky". Built for the BBC micro:bit
